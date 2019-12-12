a207cb72ea37f917923987cd605cd651b74860c2
GabrielNumaX <37051017+GabrielNumaX@users.noreply.github>
2019-12-11 22:21:01 -0300

    Updates

---
+++ precache-manifest.eb43f60dd1734a78ddf90fd574947b9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20ef04c1821396248a562dea7badeb42",
+    "url": "/react-e-commerce/index.html"
+  },
+  {
+    "revision": "b9c6b6b73d6c5dde1699",
+    "url": "/react-e-commerce/static/css/main.4fca626e.chunk.css"
+  },
+  {
+    "revision": "af4a4a2b07691eadab02",
+    "url": "/react-e-commerce/static/js/2.fc9cc815.chunk.js"
+  },
+  {
+    "revision": "b9c6b6b73d6c5dde1699",
+    "url": "/react-e-commerce/static/js/main.31f79a2a.chunk.js"
+  },
+  {
+    "revision": "805a874a55fc1d9015e9",
+    "url": "/react-e-commerce/static/js/runtime-main.cc6c1b5d.js"
+  },
+  {
+    "revision": "c554f19c5f3c37f561edffe618037ba5",
+    "url": "/react-e-commerce/static/media/shopping-cart-png.c554f19c.jpg"
+  }
+]);
